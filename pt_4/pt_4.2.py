d45fc8fddfde2fe9331db94828b938024f7102c9
ananfee <136957108+ananfee@users.noreply.github.com>
2023-07-21 14:58:51 +0700

    Update pt_4.2.py
    change by pep8

--- pt_4/pt_4.2.py
+++ pt_4/pt_4.2.py
@@ -1,25 +1,10 @@
-def find_combinations(candidates1, target1, current_combination, combinations1):
-    if target == 0:
-        combinations.append(current_combination)
-        return
-
-    if target < 0:
-        return
-
-    for i in range(len(candidates1)):
-        new_combination = current_combination + [candidates1[i]]
-        find_combinations(candidates1[i:], target1 - candidates1[i], new_combination, combinations1)
+def palindrome(string):
+    reversed_string = string[::-1]
+    return string == reversed_string
 
 
-def get_combinations():
-    candidates.sort()
-    combinations1 = []
-    find_combinations(candidates, target, [], combinations1)
-    return combinations1
-
-
-candidates = [2, 3, 6, 7]
-target = 7
-combinations = get_combinations(candidates, target1)
-for combination in combinations:
-    print(combination)
+s = str(input('Введите строку: '))
+for i in range(0, len(s)):
+    for j in range(i + 1, len(s) + 1):
+        if palindrome(s[i:j]) and len(s[i:j]) != 1:
+            print(s[i:j])
