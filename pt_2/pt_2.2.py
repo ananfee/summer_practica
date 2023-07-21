e6e49576f905aead541a214c0290066aa95eb9d8
ananfee <136957108+ananfee@users.noreply.github.com>
2023-07-21 15:53:45 +0700

    Update pt_2.2.py

--- pt_2/pt_2.2.py
+++ pt_2/pt_2.2.py
@@ -1,12 +1,13 @@
-tv_shows = ["Орел и решка", "Спокойной ночи, малыши", "Давай поженимся", "Беременна в 16"]
-
-print("Список передач:")
-print('\n'.join(tv_shows))
-
-new_show = input("Введите название новой передачи: ")
-position = int(input("Введите позицию для вставки (от 1 до {}): ".format(len(tv_shows) + 1)))
-
-tv_shows.insert(position - 1, new_show)
-
-print("Обновленный список передач:")
-print('\n'.join(tv_shows))
+arr = ['Мужское женское', '4 свадьбы',
+       'Пусть говорят', 'Орёл и решка']
+for i in range(len(arr)):
+    s = arr[i]
+    print(s)
+name = str(input('Введите название: '))
+num = int(input('Введите номер позиции: '))
+print()
+arr.insert(num - 1, name)
+for i in range(len(arr)):
+    s = arr[i]
+    print(s)
+  
