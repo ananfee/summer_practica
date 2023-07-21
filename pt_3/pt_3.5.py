e674cf560e308789f562056fef8fe47c6d222e21
ananfee <136957108+ananfee@users.noreply.github.com>
2023-07-21 17:26:58 +0700

    Update pt_3.5.py

--- pt_3/pt_3.5.py
+++ pt_3/pt_3.5.py
@@ -1,11 +1,12 @@
-def is_prime(n1):
-    if n1 <= 1:
-        return False
-    for i in range(2, int(n1 ** 0.5) + 1):
-        if n1 % i == 0:
-            return False
-    return True
+def not_prime(x):
+    if x <= 1:
+        return True
+    for i in range(2, int(x**0.5) + 1):
+        if x % i == 0:
+            return True
+    return False
 
-n = 20
-numbers = [x for x in range(n) if not is_prime(x)]
-print(numbers)
+
+n = int(input("Введите число N: "))
+arr = [i for i in range(n) if not_prime(i)]
+print("Не простые числа от 0 до N", arr)
